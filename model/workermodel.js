d48df88e2f0c7fe9469575edb2a3c50c505d2e2a
gnaa <gnana.uppunuthula@gmail.com>
2024-10-29 23:27:05 +0530

    added sold button,modified viewmore pic display

--- model/workermodel.js
+++ model/workermodel.js
@@ -1,27 +1,27 @@
 let mongoose = require('mongoose');
 
 let workerSchema = mongoose.Schema({
-    email: String,
-    password: String,
-    name: String,
-    uniqueId: { type: String, unique: true },
-    phone: Number,
-    books: [{
-        title: String,
-        author: String,
-        price: Number,
-        category: String,
-        book_images: [{ 
-            filename: { type: String },
-            contentType: { type: String },
-            uploadDate: { type: Date },
-            metadata: { type: Object }
-        }],
-        condition: String
-    }],
-    role: { type: String, enum: ['client', 'worker'], default: 'worker' }
+  email: String,
+  password: String,
+  name: String,
+  uniqueId: { type: String, unique: true },
+  phone: Number,
+  books: [{
+    title: String,
+    author: String,
+    price: Number,
+    category: String,
+    sold: { type: String, enum: ['yes', 'no'], default: 'no' },
+    book_image: { // Change to a single image object
+      filename: { type: String },
+      contentType: { type: String },
+      uploadDate: { type: Date },
+      metadata: { type: Object }
+    },
+    condition: String
+  }],
+  role: { type: String, enum: ['client', 'worker'], default: 'worker' }
 });
 
 let sellermodel = mongoose.model('workeregform', workerSchema);
-
 module.exports = sellermodel;
